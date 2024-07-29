67ebd3f46b45f26bf00fa2c483d90b66d592ce0c
MdBillG <mdb8854@gmail.com>
2024-07-29 21:41:17 +0530

    working on alpabet soup

--- very-easy/additive-inverse/code.js
+++ very-easy/additive-inverse/code.js
@@ -1,5 +1,9 @@
 function additiveInverse(arr) {
   // Your code here.
+
+  return arr.map(n => n * - 1)
+
+
 }
 
 module.exports = additiveInverse;
--- very-easy/alphabet-soup/code.js
+++ very-easy/alphabet-soup/code.js
@@ -1,5 +1,7 @@
 function AlphabetSoup(str) {
   // Your code here.
+
+  return str.split("").sort().join("")
 }
 
 module.exports = AlphabetSoup;
