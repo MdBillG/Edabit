53597c596c0fb888d91de1386d812e44652c938e
MdBillG <mdb8854@gmail.com>
2024-07-30 21:29:50 +0530

    working on reverse

--- very-easy/check-if-a-number-is-a-palindrome/code.js
+++ very-easy/check-if-a-number-is-a-palindrome/code.js
@@ -1,5 +1,12 @@
 function isPalindrome(n) {
   // Your code here.
+
+  const str = n.toString();
+
+  const reverterfStr = str.split('').reverse().join('')
+
+  return str === reverterfStr
 }
+
 
 module.exports = isPalindrome;
