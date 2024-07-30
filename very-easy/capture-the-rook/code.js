c662e1b9f59111f6e5e742d944e41a4dc0b75cfa
MdBillG <mdb8854@gmail.com>
2024-07-30 20:58:37 +0530

    working on rook

--- very-easy/capture-the-rook/code.js
+++ very-easy/capture-the-rook/code.js
@@ -1,5 +1,10 @@
 function canCapture([yourRook, opponentsRook]) {
   // Your code here.
+  const [youRookRow, youRookColumn] = [yourRook[0], yourRook[1]]
+  const [opponentsRookRow, opponentsRookColumn] = [opponentsRook[0], opponentsRook[1]]
+
+  return youRookRow === opponentsRookRow || youRookColumn === opponentsRookColumn
+
 }
 
 module.exports = canCapture;
