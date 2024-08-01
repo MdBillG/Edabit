7f8c5fc4d7c8671cb107249f820ee7d13cf5df10
MdBillG <mdb8854@gmail.com>
2024-08-01 23:19:19 +0530

    working on months

--- very-easy/convert-number-to-corresponding-month-name/code.js
+++ very-easy/convert-number-to-corresponding-month-name/code.js
@@ -1,5 +1,10 @@
 function month_name(num) {
   // Your code here.
-}
+  const months = [
+    "January", "February", "March", "April", "May", "June",
+    "July", "August", "September", "October", "November", "December"
+  ];
 
+  return months[num - 1]
+}
 module.exports = month_name;
