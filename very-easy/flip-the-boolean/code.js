55bf8b72f4b8d8c401a58c7aa81ebccb92a163e3
MdBillG <mdb8854@gmail.com>
2024-08-04 20:33:04 +0530

    working on boolean flip

--- very-easy/flip-the-boolean/code.js
+++ very-easy/flip-the-boolean/code.js
@@ -1,5 +1,11 @@
 function reverse(bool) {
   // Your code here.
+  if (bool === true)
+    return false;
+  else if (bool === false)
+    return true
+  else
+    return "boolean expected"
 }
 
 module.exports = reverse;
