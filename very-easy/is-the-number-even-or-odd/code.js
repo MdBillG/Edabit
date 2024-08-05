94300cc42e4964d54807676ae2a0b0bba2d06105
MdBillG <mdb8854@gmail.com>
2024-08-05 20:38:30 +0530

    working on zero

--- very-easy/is-the-number-even-or-odd/code.js
+++ very-easy/is-the-number-even-or-odd/code.js
@@ -1,5 +1,14 @@
 function isEvenOrOdd(num) {
   // Your code here.
+
+  if (num % 2 === 0) {
+    return "even"
+  }
+  else {
+    return "odd"
+  }
+
+
 }
 
 module.exports = isEvenOrOdd;
--- very-easy/is-the-number-less-than-or-equal-to-zero/code.js
+++ very-easy/is-the-number-less-than-or-equal-to-zero/code.js
@@ -1,5 +1,6 @@
 function lessThanOrEqualToZero(num) {
   // Your code here.
+  return num <= 0
 }
 
 module.exports = lessThanOrEqualToZero;
