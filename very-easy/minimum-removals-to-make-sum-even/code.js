7d76f0fc44ce00adddc77b0a7610b7f5f000f111
MdBillG <mdb8854@gmail.com>
2024-08-06 21:27:16 +0530

    minimum removal

--- very-easy/minimum-removals-to-make-sum-even/code.js
+++ very-easy/minimum-removals-to-make-sum-even/code.js
@@ -1,5 +1,16 @@
 function minimumRemovals(arr) {
   // Your code here.
+  const sum = arr.reduce((a, b) => a + b, 0)
+
+  if (sum % 2 === 0)
+    return 0
+
+  for (let i = 0; i <= arr.length; i++) {
+    if (arr[i] !== 0) {
+      return 1
+    }
+  }
+
 }
 
 module.exports = minimumRemovals;
