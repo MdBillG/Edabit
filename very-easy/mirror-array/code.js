2401a3737c7d4f0b5e1c0d777ce1696ee972eacf
MdBillG <mdb8854@gmail.com>
2024-08-06 23:14:10 +0530

    working on mirror

--- very-easy/mirror-array/code.js
+++ very-easy/mirror-array/code.js
@@ -1,5 +1,10 @@
 function mirror(arr) {
   // Your code here.
+  let mainArray = [...arr]
+  const pop = mainArray.pop()
+  let reverse = mainArray.reverse()
+
+  return [...arr, ...mainArray]
 }
 
 module.exports = mirror;
