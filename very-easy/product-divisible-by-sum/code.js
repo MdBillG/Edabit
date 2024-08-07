d8ec62349a20fa010f4770531e4a19c579836c7d
MdBillG <mdb8854@gmail.com>
2024-08-07 20:13:38 +0530

    working on odd of all integers

--- very-easy/product-divisible-by-sum/code.js
+++ very-easy/product-divisible-by-sum/code.js
@@ -1,5 +1,8 @@
 function divisible(arr) {
   // Your code here.
+  const sum = arr.reduce((a, b) => a + b)
+  const product = arr.reduce((a, b) => a * b)
+  return product % sum === 0
 }
 
 module.exports = divisible;
--- very-easy/product-of-all-odd-integers/code.js
+++ very-easy/product-of-all-odd-integers/code.js
@@ -1,5 +1,7 @@
 function oddProduct(arr) {
   // Your code here.
+  const odd = arr.filter(n => n % 2 !== 0)
+  return odd.reduce((a, b) => a * b)
 }
 
 module.exports = oddProduct;
