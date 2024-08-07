d2da63db6b28ed66e34c04155d51309f9a61391b
MdBillG <mdb8854@gmail.com>
2024-08-07 12:44:41 +0530

    worked on n samllest element

--- very-easy/nth-smallest-element/code.js
+++ very-easy/nth-smallest-element/code.js
@@ -1,5 +1,11 @@
 function nthSmallest(arr, n) {
-  // Your code here.
+  arr.sort((a, b) => a - b);
+
+  if (n <= 0 || n > arr.length) {
+    return null;
+  }
+
+  return arr[n - 1];
 }
 
 module.exports = nthSmallest;
