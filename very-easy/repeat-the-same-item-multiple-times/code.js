7fbbe3c3be4bd00ff2b6e231fa076e72057e9f54
MdBillG <mdb8854@gmail.com>
2024-08-16 19:14:51 +0530

    same element multiple times

--- very-easy/repeat-the-same-item-multiple-times/code.js
+++ very-easy/repeat-the-same-item-multiple-times/code.js
@@ -1,5 +1,12 @@
 function repeat(item, times) {
   // Your code here.
+  const result = []
+  for (let i = 0; i < times; i++) {
+    result.push(item)
+
+  }
+  return result
+
 }
 
 module.exports = repeat;
