5b392c66161fafa3ed5d0dc0c5fb4612692c139b
MdBillG <mdb8854@gmail.com>
2024-08-16 19:24:16 +0530

    repeat n times

--- very-easy/repeating-letters-n-times/code.js
+++ very-easy/repeating-letters-n-times/code.js
@@ -1,5 +1,13 @@
 function repeat(str, n) {
   // Your code here.
+  let result = []
+  for (let i = 0; i < str.length; i++) {
+    let char = str[i]
+    for (let j = 0; j < n; j++) {
+      result += char
+    }
+  }
+  return result
 }
 
 module.exports = repeat;
