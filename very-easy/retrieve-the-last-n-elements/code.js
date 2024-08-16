9bb1ae44336d711e7d1f2db348c01197bbf7f14a
MdBillG <mdb8854@gmail.com>
2024-08-16 21:16:09 +0530

    working on n last elements

--- very-easy/retrieve-the-last-n-elements/code.js
+++ very-easy/retrieve-the-last-n-elements/code.js
@@ -1,5 +1,11 @@
-function last(a, n) {
-  // Your code here.
+function last(arr, n) {
+  if (n > arr.length) {
+    return "invalid";
+  }
+  if (n === 0) {
+    return [];
+  }
+  return arr.slice(-n);
 }
 
 module.exports = last;
