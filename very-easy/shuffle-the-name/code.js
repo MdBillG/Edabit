f94b889517bfdb5fae1a944306ea7821ae9271cd
MdBillG <mdb8854@gmail.com>
2024-08-17 19:28:55 +0530

    shuffle

--- very-easy/shuffle-the-name/code.js
+++ very-easy/shuffle-the-name/code.js
@@ -1,5 +1,7 @@
 function nameShuffle(str) {
-  // Your code here.
+  // Your code here. 
+  const [firstName, LastName] = str.split(' ')
+  return `${LastName} ${firstName}`
 }
 
 module.exports = nameShuffle;
