52f0b720e2981692602d5ed587f2b3a1297d10cb
MdBillG <mdb8854@gmail.com>
2024-08-17 19:36:30 +0530

    smaller string number

--- very-easy/smaller-string-number/code.js
+++ very-easy/smaller-string-number/code.js
@@ -1,5 +1,11 @@
-function smallerNum(n1, n2) {
-  // Your code here.
+function smallerNum(num1, num2) {
+  if (num1.length < num2.length) {
+    return num1;
+  } else if (num1.length > num2.length) {
+    return num2;
+  } else {
+    return num1 < num2 ? num1 : num2;
+  }
 }
 
 module.exports = smallerNum;
