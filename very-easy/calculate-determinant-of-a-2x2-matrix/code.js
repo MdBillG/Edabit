730e1421daf51b0f1f0bd576f74f2aa2869672cc
MdBillG <mdb8854@gmail.com>
2024-08-18 21:03:16 +0530

    matirix

--- very-easy/calculate-determinant-of-a-2x2-matrix/code.js
+++ very-easy/calculate-determinant-of-a-2x2-matrix/code.js
@@ -1,5 +1,12 @@
 function calcDeterminant(matrix) {
   // Your code here.
+  console.log("mad", matrix)
+
+  let a = matrix[0][0]
+  let b = matrix[0][1]
+  let c = matrix[1][0]
+  let d = matrix[1][1]
+  return [a * d] - [b * c]
 
 
 }
