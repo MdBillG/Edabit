0c6b90fa76e1862002be6c539077599d4f19489d
MdBillG <mdb8854@gmail.com>
2024-08-18 14:30:19 +0530

    abbreviations

--- very-easy/state-names-and-abbreviations/code.js
+++ very-easy/state-names-and-abbreviations/code.js
@@ -1,5 +1,9 @@
 function filterStateNames(arr, type) {
-  // Your code here.
+
+  if (type === "full")
+    return arr.filter(x => x.length > 2)
+  else
+    return arr.filter(x => x.length <= 2)
 }
 
 module.exports = filterStateNames;
