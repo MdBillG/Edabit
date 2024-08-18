ff80b14f065f1080281379397ea960bc3346b573
MdBillG <mdb8854@gmail.com>
2024-08-18 16:41:21 +0530

    user typed

--- very-easy/typing-game/code.js
+++ very-easy/typing-game/code.js
@@ -1,5 +1,14 @@
 function correctStream(user, correct) {
-  // Your code here.
+  let correct2 = []
+  for (let i = 0; i < user.length; i++) {
+    if (user[i] === correct[i]) {
+      correct2.push(1)
+    } else {
+      correct2.push(-1)
+    }
+  }
+
+  return correct2
 }
 
 module.exports = correctStream;
