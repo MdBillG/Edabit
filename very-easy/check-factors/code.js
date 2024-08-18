34504776ae465fb9a92d1f82723a7e4d0d41bee3
MdBillG <mdb8854@gmail.com>
2024-08-18 21:15:17 +0530

    factores

--- very-easy/check-factors/code.js
+++ very-easy/check-factors/code.js
@@ -1,6 +1,11 @@
 function checkFactors(factors, num) {
-  // Ensure num is positive
-
+  for (let i = 0; i < factors.length; i++) {
+    if (num % factors[i] !== 0) {
+      console.log(`Failed at factor ${factors[i]} for number ${num}`);
+      return false;
+    }
+  }
+  return true;
 }
 
 module.exports = checkFactors;
