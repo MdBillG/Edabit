2380ec1c00f972db0400b7b0af832b5bc1ede858
MdBillG <mdb8854@gmail.com>
2024-08-19 13:04:42 +0530

    number is palindrome

--- very-easy/check-factors/code.js
+++ very-easy/check-factors/code.js
@@ -1,7 +1,6 @@
 function checkFactors(factors, num) {
   for (let i = 0; i < factors.length; i++) {
     if (num % factors[i] !== 0) {
-      console.log(`Failed at factor ${factors[i]} for number ${num}`);
       return false;
     }
   }
--- very-easy/check-if-a-number-is-a-palindrome/code.js
+++ very-easy/check-if-a-number-is-a-palindrome/code.js
@@ -1,4 +1,15 @@
-function isPalindrome(n) {
+function isPalindrome(num) {
+    if (num < 0) return false;
+
+    let reversed = 0;
+    let original = num;
+
+    while (num > 0) {
+        reversed = reversed * 10 + num % 10;
+        num = Math.floor(num / 10);
+    }
+
+    return original === reversed;
 
 }
 
