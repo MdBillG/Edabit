b56ca2d6adb0dc8010dbb91bf8fbe58f24f37d89
MdBillG <mdb8854@gmail.com>
2024-08-19 18:55:44 +0530

    exist higher

--- very-easy/even-number-generator/code.js
+++ very-easy/even-number-generator/code.js
@@ -1,12 +1,14 @@
 function findEvenNums(n) {
   // Your code here.
   let even = []
-  for (let i = 1; i <= n; i += 1) {
-    if (i % 2 === 0) {
-      even.push(i)
-    }
-  }
-  return even
+  // for (let i = 1; i <= n; i += 1) {
+  //   if (i % 2 === 0) {
+  //     even.push(i)
+  //   }
+  // }
+  // return even
+
+  for (let num of n)
 }
 
 module.exports = findEvenNums;
--- very-easy/exists-a-number-higher/code.js
+++ very-easy/exists-a-number-higher/code.js
@@ -1,7 +1,11 @@
 function existsHigher(arr, n) {
   // Your code here.
-
-
+  for (let num of arr) {
+    if (num >= n) {
+      return true
+    }
+  }
+  return false
 }
 
 module.exports = existsHigher;
