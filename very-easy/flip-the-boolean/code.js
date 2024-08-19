c64ba4059143f0872820839388b069b50e92c8c6
MdBillG <mdb8854@gmail.com>
2024-08-19 22:04:55 +0530

    revreser bool

--- very-easy/flip-the-boolean/code.js
+++ very-easy/flip-the-boolean/code.js
@@ -1,5 +1,9 @@
 function reverse(bool) {
-
+    if (bool === true)
+        return false
+    else if (bool === false)
+        return true
+    return "boolean expected"
 }
 
 module.exports = reverse;
