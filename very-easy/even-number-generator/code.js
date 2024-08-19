3f8396d90f4010bbe8e18846c467e281ab0087eb
MdBillG <mdb8854@gmail.com>
2024-08-19 16:47:21 +0530

    even number geenrator

--- very-easy/even-number-generator/code.js
+++ very-easy/even-number-generator/code.js
@@ -1,7 +1,12 @@
 function findEvenNums(n) {
   // Your code here.
-
-
+  let even = []
+  for (let i = 1; i <= n; i += 1) {
+    if (i % 2 === 0) {
+      even.push(i)
+    }
+  }
+  return even
 }
 
 module.exports = findEvenNums;
