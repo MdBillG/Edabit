87a930821ee9ead0fe9158b252cab5d9cfab052a
MdBillG <mdb8854@gmail.com>
2024-08-19 22:14:24 +0530

    genrate numbers

--- very-easy/generate-a-countdown-of-numbers-in-an-array/code.js
+++ very-easy/generate-a-countdown-of-numbers-in-an-array/code.js
@@ -1,6 +1,14 @@
 function countdown(start) {
   // Your code here.
 
+  let numbers = []
+  for (let i = start; i >= 0; i--) {
+    numbers.push(i)
+  }
+  console.log("object", "numbers", numbers)
+
+  return numbers
+
 
 }
 
