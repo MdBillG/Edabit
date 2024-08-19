67c35ff5b82bd79e37813d6b47c970e9acf71dfb
MdBillG <mdb8854@gmail.com>
2024-08-19 13:15:31 +0530

    identical

--- very-easy/check-if-a-string-contains-only-identical-characters/code.js
+++ very-easy/check-if-a-string-contains-only-identical-characters/code.js
@@ -1,5 +1,14 @@
 function isIdentical(s) {
 
+    let n = s.length
+    for (let i = 1; i < n; i++) {
+        if (s[i] !== s[0])
+            return false
+
+
+    }
+    return true
+
 }
 
 module.exports = isIdentical;
