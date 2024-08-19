7bb6911536565224c24abd4e44b8de1933abb640
MdBillG <mdb8854@gmail.com>
2024-08-19 18:36:17 +0530

    even partitoin

--- very-easy/even-odd-partition/code.js
+++ very-easy/even-odd-partition/code.js
@@ -1,5 +1,16 @@
 function evenOddPartition(arr) {
+  let odd = []
+  let even = []
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] % 2 !== 0) {
+      odd.push(arr[i])
+    }
+    else {
+      even.push(arr[i])
 
+    }
+  }
+  return [even, odd];
 
 }
 
