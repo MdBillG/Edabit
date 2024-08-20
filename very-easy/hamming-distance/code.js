3d18e35f999337661af921f21de77bee3291d01c
MdBillG <mdb8854@gmail.com>
2024-08-20 23:18:52 +0530

    working on humming distance

--- very-easy/hamming-distance/code.js
+++ very-easy/hamming-distance/code.js
@@ -1,7 +1,13 @@
 function hammingDistance(str1, str2) {
   // Your code here.
 
-
+  let distance = 0
+  for (let i = 0; i < str1.length; i++) {
+    if (str1[i] !== str2[i]) {
+      distance += 1
+    }
+  }
+  return distance
 }
 
 module.exports = hammingDistance;
