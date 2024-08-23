88336631a3f36afbbab38b23565cca4a6db16a2a
MdBillG <mdb8854@gmail.com>
2024-08-23 20:44:54 +0530

    lexographically

--- very-easy/lexicographically-first-and-last/code.js
+++ very-easy/lexicographically-first-and-last/code.js
@@ -1,4 +1,6 @@
 function firstAndLast(s) {
-
+    const sort = s.split('').sort().join('')
+    const reverse = sort.split('').reverse().join('')
+    return [sort, reverse]
 }
 module.exports = firstAndLast;
