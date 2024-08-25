e3494a35be1d8b8fd45ca935d087fdb89363764a
MdBillG <mdb8854@gmail.com>
2024-08-25 16:38:23 +0530

    missing third angle

--- very-easy/missing-third-angle/code.js
+++ very-easy/missing-third-angle/code.js
@@ -1,5 +1,11 @@
 function missingAngle(angle1, angle2) {
 
+  const missingAngle = 180 - (angle1 + angle2)
+
+  return missingAngle > 90 ? 'obtuse' : missingAngle < 90 ? 'acute' : 'right'
+
+
+
 }
 
 module.exports = missingAngle;
