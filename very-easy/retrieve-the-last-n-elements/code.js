c0129f892afdf4590a1f7a94631eb96c584ca4a2
MdBillG <mdb8854@gmail.com>
2024-08-26 12:09:13 +0530

    retrive

--- very-easy/retrieve-the-last-n-elements/code.js
+++ very-easy/retrieve-the-last-n-elements/code.js
@@ -1,5 +1,12 @@
 function last(arr, n) {
+    if (n > arr.length) {
+        return 'invalid'
+    }
+    else if (n === 0) {
+        return []
+    }
 
+    return arr.slice(-n)
 }
 
 module.exports = last;
