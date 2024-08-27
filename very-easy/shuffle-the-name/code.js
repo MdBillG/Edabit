f07ee3ccb394693ebdd68590ffef1b5c86bd15f2
MdBillG <mdb8854@gmail.com>
2024-08-27 12:52:22 +0530

    first name and lastname

--- very-easy/shuffle-the-name/code.js
+++ very-easy/shuffle-the-name/code.js
@@ -1,6 +1,8 @@
 function nameShuffle(str) {
   // Your code here. 
-
+  return str.split(' ')[1] + " " + str.split(' ')[0]
+  // const [firstName, lastName] = str.split(' ')
+  // return `${lastName} ${firstName}`
 }
 
 module.exports = nameShuffle;
