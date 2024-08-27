9d9d2e2b6c1169edb2e0311abf165a4ec35eaa01
MdBillG <mdb8854@gmail.com>
2024-08-27 22:06:07 +0530

    sum of arthemetic sequence

--- easy/add-up-the-numbers-from-a-single-number/code.js
+++ easy/add-up-the-numbers-from-a-single-number/code.js
@@ -1,5 +1,15 @@
 function addUp(num) {
-  // Your code here.
+  // Your code here.l
+  // let a = []
+
+  // console.log("object", num)
+  // for (let i = 0; i <= num; i++) {
+  //   a.push(i)
+  // }
+
+  // return a.reduce((a, b) => a + b)
+
+  return (num * (num + 1)) / 2
 }
 
 module.exports = addUp;
