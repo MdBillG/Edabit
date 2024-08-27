a3f6bfca54e86feec82902ebfa05352cb1dd56f8
MdBillG <mdb8854@gmail.com>
2024-08-27 13:52:54 +0530

    no of unique chars

--- very-easy/total-number-of-unique-characters/code.js
+++ very-easy/total-number-of-unique-characters/code.js
@@ -1,6 +1,13 @@
 function countUnique(s1, s2) {
   // Your code here.
-
+  const s3 = s1 + s2
+  let unique = ''
+  for (let i = 0; i < s3.length; i++) {
+    if (unique.includes(s3[i]) === false) {
+      unique += s3[i]
+    }
+  }
+  return unique.length
 
 }
 
