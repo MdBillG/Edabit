de8ebbe87c9a886778f5e71c116f415dde38121d
MdBillG <mdb8854@gmail.com>
2024-08-27 14:04:53 +0530

    user typed words

--- very-easy/typing-game/code.js
+++ very-easy/typing-game/code.js
@@ -1,4 +1,15 @@
 function correctStream(user, correct) {
+    let correct2 = []
+
+    for (let i = 0; i < user.length; i++) {
+        if (user[i] === correct[i]) {
+            correct2.push(1)
+        }
+        else
+            correct2.push(-1)
+    }
+
+    return correct2
 
 }
 
