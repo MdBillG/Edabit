5a68f58d183938b8e7dccb41b753c5b9283c95ac
MdBillG <mdb8854@gmail.com>
2024-08-28 12:58:30 +0530

    mean

--- easy/calculate-the-mean/code.js
+++ easy/calculate-the-mean/code.js
@@ -1,5 +1,8 @@
 function mean(arr) {
   // Your code here.
+  const sum = arr.reduce((a, b) => a + b, 0);
+  // Calculate the mean and round to two decimal places
+  return parseFloat((sum / arr.length).toFixed(2));
 }
 
 module.exports = mean;
