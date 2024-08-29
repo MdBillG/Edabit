4973ed581c6a7ef27e80874d303374aa7f78902d
MdBillG <mdb8854@gmail.com>
2024-08-29 22:36:39 +0530

    chat room

--- easy/chat-room-status/code.js
+++ easy/chat-room-status/code.js
@@ -1,5 +1,17 @@
 function chatroomStatus(users) {
   // Your code here.
+  const numUsers = users.length;
+
+  if (numUsers === 0) {
+    return "no one online";
+  } else if (numUsers === 1) {
+    return `${users[0]} online`;
+  } else if (numUsers === 2) {
+    return `${users[0]} and ${users[1]} online`;
+  } else {
+    return `${users[0]}, ${users[1]} and ${numUsers - 2} more online`;
+  }
+
 }
 
 module.exports = chatroomStatus;
