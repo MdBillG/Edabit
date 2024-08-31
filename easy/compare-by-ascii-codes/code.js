3aee10e424d26962665cc635a88635e5cb00617b
MdBillG <mdb8854@gmail.com>
2024-08-31 14:56:32 +0530

    ascii value

--- easy/compare-by-ascii-codes/code.js
+++ easy/compare-by-ascii-codes/code.js
@@ -1,5 +1,13 @@
 function asciiSort(arr) {
   // Your code here.
+  const getAsciiSum = (word) => {
+    return word.split('').reduce((sum, chr) => sum + chr.charCodeAt(0), 0)
+  }
+  const sum1 = getAsciiSum(arr[0])
+  const sum2 = getAsciiSum(arr[1])
+
+  console.log("asdasf", sum1, sum2)
+  return sum1 < sum2 ? arr[0] : arr[1]
 }
 
 module.exports = asciiSort;
