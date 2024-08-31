8858f0c2764d728f1b12ad8701b9fe3ac24f30eb
MdBillG <mdb8854@gmail.com>
2024-08-31 18:35:55 +0530

    2d

--- easy/count-ones-in-a-2d-array/code.js
+++ easy/count-ones-in-a-2d-array/code.js
@@ -1,5 +1,8 @@
 function countOnes(matrix) {
   // Your code here.
+  return matrix.reduce((total, row) => {
+    return total + row.filter(num => num === 1).length
+  }, 0)
 }
 
 module.exports = countOnes;
