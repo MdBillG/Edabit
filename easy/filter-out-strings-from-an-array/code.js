c963b48458ed18496b52e78baa7d70de716ac256
MdBillG <mdb8854@gmail.com>
2024-09-01 15:01:05 +0530

    filterArray

--- easy/filter-out-strings-from-an-array/code.js
+++ easy/filter-out-strings-from-an-array/code.js
@@ -1,5 +1,17 @@
 function filterArray(arr) {
   // Your code here.
+  let newArray = []
+
+  for (let i = 0; i < arr.length; i++) {
+    if (typeof arr[i] !== 'string') {
+      newArray.push(arr[i])
+    }
+  }
+
+  return newArray
+
+
+
 }
 
 module.exports = filterArray;
