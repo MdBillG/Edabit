a0771779b800ba878f4df983568e4e1b448d90ab
MdBillG <mdb8854@gmail.com>
2024-09-04 16:19:48 +0530

    min,max,sequence

--- easy/find-the-minimum-maximum-length-and-average-values/code.js
+++ easy/find-the-minimum-maximum-length-and-average-values/code.js
@@ -1,5 +1,12 @@
 function minMaxLengthAverage(arr) {
   // Your code here.
+  const minValue = Math.min(...arr)
+  const maxValue = Math.max(...arr)
+  const sequnceLength = arr.length
+  const averageValue = arr.reduce((a, b) => a + b, 0) / sequnceLength
+  console.log("averageValue", averageValue)
+
+  return [minValue, maxValue, sequnceLength, averageValue]
 }
 
 module.exports = minMaxLengthAverage;
