5e452ac77b9b558545f58da4fdc43de35f6040a6
MdBillG <mdb8854@gmail.com>
2024-09-04 22:48:25 +0530

    greatest common divisor

--- easy/gcd-of-two-numbers/code.js
+++ easy/gcd-of-two-numbers/code.js
@@ -1,5 +1,13 @@
 function gcd(n1, n2) {
   // Your code here.
+  let smaller = Math.min(n1, n2)
+  let hcf = 1
+  for (let i = 0; i <= smaller; i++) {
+    if (n1 % i === 0 && n2 % i === 0) {
+      hcf = i
+    }
+  }
+  return hcf
 }
 
 module.exports = gcd;
