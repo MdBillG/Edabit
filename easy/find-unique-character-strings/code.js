53ab2fc526c737e835276a81947a5807f3b7d7de
MdBillG <mdb8854@gmail.com>
2024-09-04 20:57:05 +0530

    first index lastindex

--- easy/find-unique-character-strings/code.js
+++ easy/find-unique-character-strings/code.js
@@ -1,5 +1,29 @@
 function filterUnique(arr) {
   // Your code here.
+  const result = [];
+
+  for (let i = 0; i < arr.length; i++) {
+    const str = arr[i];
+    const charSet = new Set();
+    let isUnique = true;
+
+    for (let j = 0; j < str.length; j++) {
+      const char = str[j];
+      if (charSet.has(char)) {
+        isUnique = false;
+        break;
+      }
+      charSet.add(char);
+    }
+
+    if (isUnique) {
+      result.push(str);
+    }
+  }
+
+  return result;
+
+
 }
 
 module.exports = filterUnique;
--- easy/first-and-last-index/code.js
+++ easy/first-and-last-index/code.js
@@ -1,5 +1,31 @@
 function charIndex(word, char) {
   // Your code here.
+
+  // const firstIndex = word.indexOf(char);
+  // const lastIndex = word.lastIndexOf(char);
+
+  // if (firstIndex === -1) {
+  //   return undefined;
+  // }
+
+  // return [firstIndex, lastIndex];
+
+  let fistIndex = -1
+  let lastIndex = -1
+  for (let i = 0; i < word.length; i++) {
+    if (word[i] === char) {
+      if (fistIndex === -1) {
+        fistIndex = i
+      }
+      lastIndex = i
+    }
+  }
+
+  if (fistIndex !== -1) {
+    return [fistIndex, lastIndex];
+  } else {
+    return undefined;
+  }
 }
 
 module.exports = charIndex;
