fa8716f422759a3983f54fed03a2595aad083518
MdBillG <user.email>
2025-02-13 17:49:16 +0530

    worked on invlude

--- very-easy/check-if-an-array-includes-an-element/code.js
+++ very-easy/check-if-an-array-includes-an-element/code.js
@@ -1,5 +1,18 @@
 function check(arr, el) {
   // Your code here.
+
+  // modren javascript
+  // return arr.includes(el);
+
+  // for loop
+
+for(let i = 0; i < arr.length; i++){
+  if(arr[i] === el){
+    return true;
+  } 
 }
+return false
+}
+
 
 module.exports = check;
