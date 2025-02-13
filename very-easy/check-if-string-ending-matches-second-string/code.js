f85de2d86e0f604cae88601b24b6a8a43a905810
MdBillG <user.email>
2025-02-13 18:57:04 +0530

    checke the ending

--- very-easy/check-if-string-ending-matches-second-string/code.js
+++ very-easy/check-if-string-ending-matches-second-string/code.js
@@ -1,7 +1,13 @@
 function checkEnding(str1, str2) {
   // Your code here.
-  return str1.endsWith(str2);
+  // return str1.endsWith(str2);
 
+  for(let i = 0; i < str2.length; i++) {
+    if(str1[str1.length - str2.length + i] !== str2[i]) {
+      return false;
+    }
+  }
+return true
 }
 
 module.exports = checkEnding;
