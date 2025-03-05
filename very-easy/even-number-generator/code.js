e75791f6f7d7398120e86f09316598052c2dfe5f
MdBillG <user.email>
2025-03-05 22:18:42 +0530

    working on even numb ers generator

--- very-easy/even-number-generator/code.js
+++ very-easy/even-number-generator/code.js
@@ -1,6 +1,13 @@
 function findEvenNums(n) {
   // Your code here.
-
+  let allNUmbers = []
+  for(let i=2;i<=n;i++)
+  {
+allNUmbers.push(i)
+  }
+console.log(allNUmbers)
+console.log("allNUmbers.filter(x=>x%2===0)",allNUmbers.filter(x=>x%2===0))
+return allNUmbers.filter(x=>x%2===0)
 }
 
 module.exports = findEvenNums;
