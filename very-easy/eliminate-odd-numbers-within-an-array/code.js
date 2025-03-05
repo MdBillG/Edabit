3a75da646228947b485bf22f2e2bc346f84db855
MdBillG <user.email>
2025-03-05 20:02:46 +0530

    even or odd

--- very-easy/eliminate-odd-numbers-within-an-array/code.js
+++ very-easy/eliminate-odd-numbers-within-an-array/code.js
@@ -1,5 +1,14 @@
 function noOdds(arr) {
   // Your code here.
+  // return arr.filter(x=>x%2===0)
+  let even=[]
+  for(let i = 0;i <= arr.length;i++){
+    if(arr[i]%2===0){
+      even.push(arr[i])
+    }
+  }
+  console.log("even",even)
+  return even
 }
 
 module.exports = noOdds;
