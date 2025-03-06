27e426bcd2dca59b2c05bd24c94f581c3903fa82
MdBillG <user.email>
2025-03-06 12:06:08 +0530

    exists-a-higher-number

--- very-easy/exists-a-number-higher/code.js
+++ very-easy/exists-a-number-higher/code.js
@@ -1,6 +1,13 @@
 function existsHigher(arr, n) {
   // Your code here.
+  console.log("arr", arr);
 
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] >= n) {
+      return true;
+    }
+  }
+  return false;
 }
 
 module.exports = existsHigher;
