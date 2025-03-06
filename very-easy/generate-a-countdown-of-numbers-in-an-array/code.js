064fb0afbf86b07da95dade745c606b5327c7f8b
MdBillG <user.email>
2025-03-06 20:28:17 +0530

    count doen

--- very-easy/generate-a-countdown-of-numbers-in-an-array/code.js
+++ very-easy/generate-a-countdown-of-numbers-in-an-array/code.js
@@ -1,8 +1,10 @@
 function countdown(start) {
-  // Your code here.
-
-
-
+  // Your code here.===
+ let result= []
+ for (let i= start ; i>=0;i--){
+  result.push(i)
+}
+return result
 }
 
 module.exports = countdown;
