560b293590ebd6722b08f7c38f111cf5d122f911
MdBillG <user.email>
2025-03-06 20:04:05 +0530

    fix the error  muatating the error

--- very-easy/fix-the-error-mutating-arrays/code.js
+++ very-easy/fix-the-error-mutating-arrays/code.js
@@ -1,6 +1,10 @@
 function minusOne(arr) {
   // Your code here.
 
+  let newArr = arr.slice(); // Create a copy of the array
+  newArr.pop(); // Remove the last element from the copied array
+  return newArr; // Return the modified copy
+
  
 }
 
--- very-easy/fix-the-error-mutating-arrays/code.spec.js
+++ very-easy/fix-the-error-mutating-arrays/code.spec.js
@@ -2,6 +2,7 @@ const minusOne = require('./code');
 
 describe('Tests', () => {
     test('the tests', () => {
+        const x = [1, 2, 3, 4, 5];
         expect(minusOne(x)).toEqual([1, 2, 3, 4]);
         expect(minusOne(x)).toEqual([1, 2, 3, 4]);
         expect(minusOne(x)).toEqual([1, 2, 3, 4]);
