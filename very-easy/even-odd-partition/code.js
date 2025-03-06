7d49a4502845036d50c054382da9975102bc34b7
MdBillG <user.email>
2025-03-06 11:46:22 +0530

    even odd partition

--- very-easy/even-odd-partition/code.js
+++ very-easy/even-odd-partition/code.js
@@ -1,5 +1,18 @@
 function evenOddPartition(arr) {
 
+    let even = []
+    let odd = []
+
+    for(let i=0;i<=arr.length-1;i++){
+        if(arr[i]%2===0){
+            even.push(arr[i])
+        }else
+        {
+            odd.push(arr[i])
+        }
+    }
+    return [even,odd]
+
 }
 
 module.exports = evenOddPartition;
