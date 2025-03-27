faddbcf378bf386d2ceec2ff8b2d966e11942ccc
MdBillG <user.email>
2025-03-27 15:52:55 +0530

    litttle dictonary

--- very-easy/little-dictionary/code.js
+++ very-easy/little-dictionary/code.js
@@ -1,6 +1,21 @@
 function dictionary(initial, words) {
   // Your code here.
 
+  // return words.filter(x=>x.startsWith(initial))
+  let filterWords= []
+  for(let i=0;i<words.length;i++){
+    let match = true
+
+    for(let j=0;j<initial.length;j++){
+      if(words[i][j] !==initial[j]){
+        match =false
+      }
+    }
+    if(match)
+      filterWords.push(words[i])
+  }
+  return filterWords
+
 
 
 
