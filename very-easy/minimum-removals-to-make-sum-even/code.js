ff5cac736826fd4263cc55bdb740fb123662c0aa
MdBillG <user.email>
2025-03-27 18:02:28 +0530

    minimum and  reoval

--- very-easy/minimum-removals-to-make-sum-even/code.js
+++ very-easy/minimum-removals-to-make-sum-even/code.js
@@ -1,7 +1,6 @@
 function minimumRemovals(arr) {
-  // Your code here.
-
-
+  // Your code he
+  return arr.reduce((acc, cur) => acc + cur, 0) % 2 === 0 ? 0 : 1;
 }
 
 module.exports = minimumRemovals;
