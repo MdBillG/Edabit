db786faf70eaffa7409f3edff506d05ca4ffc137
MdBilG <mohammadbilal.g@royalcyber.com>
2025-04-14 15:17:25 +0530

    missing angle

--- very-easy/mirror-array/code.js
+++ very-easy/mirror-array/code.js
@@ -1,5 +1,10 @@
 function mirror(arr) {
   // Your code here.
+  const result = [...arr]
+  for (let i = arr.length - 2; i >= 0; i--) {
+    result.push(arr[i])
+  }
+  return result
 
 
 }
--- very-easy/missing-third-angle/code.js
+++ very-easy/missing-third-angle/code.js
@@ -1,5 +1,9 @@
 function missingAngle(angle1, angle2) {
 
+    console.log(angle1, angle2); // Debugging line to check the input values
+
+
+    return (angle1 + angle2) === 90 ? 'right' : 180 - (angle1 + angle2) < 90 ? 'acute' : 'obtuse';
 
 
 }
