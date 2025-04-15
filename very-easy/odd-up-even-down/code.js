136c41c17ec33db94f6fb5ab1efc18afdda7ab41
MdBilG <mohammadbilal.g@royalcyber.com>
2025-04-15 19:10:14 +0530

    palindrome

--- very-easy/odd-up-even-down/code.js
+++ very-easy/odd-up-even-down/code.js
@@ -1,4 +1,15 @@
 function transform(arr) {
+    let newArray = []
+    for (let i = 0; i <= arr.length; i++) {
+        if (arr[i] % 2 == 0) {
+            newArray.push(arr[i] + 1);
+        } else {
+            newArray.push(arr[i] - 1);
+        }
+        return newArray
+
+    }
+
 }
 
 
--- very-easy/palindrome/code.js
+++ very-easy/palindrome/code.js
@@ -1,5 +1,8 @@
 function checkPalindrome(str) {
   // Your code here.
+  console.log("object", str.split('').reverse().join(''))
+
+  return str === str.split('').reverse().join('');
 }
 
 module.exports = checkPalindrome;
