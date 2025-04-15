70270728b4a1138d74864563e4daf80efc159fd8
MdBilG <mohammadbilal.g@royalcyber.com>
2025-04-15 19:15:57 +0530

    product-deisible

--- very-easy/product-divisible-by-sum/code.js
+++ very-easy/product-divisible-by-sum/code.js
@@ -1,5 +1,9 @@
 function divisible(arr) {
 
+    let sumOfArray = arr.reduce((acc, curr) => acc + curr);
+    let productOfArray = arr.reduce((acc, curr) => acc * curr);
+    return productOfArray % sumOfArray === 0 ? true : false;
+
 
 }
 
