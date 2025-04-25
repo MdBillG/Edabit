e7ebd47d99ef143714a6187073e189df19cc8117
MdBilG <mohammadbilal.g@royalcyber.com>
2025-04-25 16:25:17 +0530

    same

--- very-easy/same-number-of-unique-elements/code.js
+++ very-easy/same-number-of-unique-elements/code.js
@@ -1,5 +1,10 @@
 function same(a1, a2) {
   // Your code here.
+
+  const uniqueA1 = new Set(a1);
+  const uniqueA2 = new Set(a2);
+  return uniqueA1.size === uniqueA2.size
+
 }
 
 module.exports = same;
