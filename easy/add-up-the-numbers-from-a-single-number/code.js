db6f3f4478ef54511ab232e78cc697ee64651d80
MdBilG <mohammadbilal.g@royalcyber.com>
2025-04-29 19:27:04 +0530

    amplyfiy-the-4

--- easy/add-up-the-numbers-from-a-single-number/code.js
+++ easy/add-up-the-numbers-from-a-single-number/code.js
@@ -1,5 +1,11 @@
 function addUp(num) {
+    let allNUmbers = []
+    for (let i = 1; i <= num; i++) {
+        allNUmbers.push(i);
+    }
+    console.log(allNUmbers);
 
+    return allNUmbers.reduce((acc, num) => acc + num)
 }
 
 module.exports = addUp;
--- easy/amplify-the-multiples-of-4/code.js
+++ easy/amplify-the-multiples-of-4/code.js
@@ -1,4 +1,14 @@
 function amplify(num) {
+    const allNumbers = [];
+    for (let i = 1; i <= num; i++) {
+        if (i % 4 === 0) {
+            allNumbers.push(i * 10);
+        } else {
+            allNumbers.push(i);
+        }
+    }
+
+    return allNumbers
 
 }
 
