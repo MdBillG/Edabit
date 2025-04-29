09805a4e4a0470955fc635be7cff54b3adb0ac76
MdBilG <mohammadbilal.g@royalcyber.com>
2025-04-29 16:48:25 +0530

    smaller string number

--- very-easy/slice-of-pie/code.js
+++ very-easy/slice-of-pie/code.js
@@ -1,5 +1,7 @@
 function equalSlices(total, people, each) {
   // Your code here.
+
+  return people * each <= total
 }
 
 module.exports = equalSlices;
--- very-easy/smaller-string-number/code.js
+++ very-easy/smaller-string-number/code.js
@@ -1,4 +1,4 @@
 function smallerNum(num1, num2) {
-
+    return Number(num1) < Number(num2) ? num1 : num2;
 }
 module.exports = smallerNum;
