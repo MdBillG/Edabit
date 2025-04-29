c09f924d3e7aa63b471f604f44f2fc2f26621c3c
MdBilG <mohammadbilal.g@royalcyber.com>
2025-04-29 19:20:29 +0530

    absoultue sum

--- easy/absolute-sum/code.js
+++ easy/absolute-sum/code.js
@@ -1,5 +1,10 @@
 function getAbsSum(arr) {
   // Your code here.
+  return arr.reduce((acc, num) => acc + Math.abs(num), 0);
+
+  // const convertingToPositive = arr.map(num => num > 0 ? num : -num);
+  // console.log(convertingToPositive);
+
 }
 
 module.exports = getAbsSum;
