7cf59e2ffdecc0a335a394d7b0da6707543e5edd
MdBilG <mohammadbilal.g@royalcyber.com>
2025-04-29 17:18:34 +0530

    isStrangePair

--- very-easy/strange-pair/code.js
+++ very-easy/strange-pair/code.js
@@ -1,4 +1,11 @@
 function isStrangePair(str1, str2) {
+
+    if (str1 === "" && str2 === "") return true;
+
+    return (
+        str1[0] === str2[str2.length - 1] &&
+        str1[str1.length - 1] === str2[0]
+    );
 }
 
 module.exports = isStrangePair;
