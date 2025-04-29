f1cb3de342c83cbcf172a62cb90ba0ad112f2427
MdBilG <mohammadbilal.g@royalcyber.com>
2025-04-29 17:01:05 +0530

    filterStateNames

--- very-easy/state-names-and-abbreviations/code.js
+++ very-easy/state-names-and-abbreviations/code.js
@@ -1,4 +1,7 @@
 function filterStateNames(arr, type) {
+    // return arr.filter((item)=>arr.len)
+
+    return type === 'abb' ? arr.filter((item) => item.length === 2) : arr.filter((item) => item.length > 2)
 }
 
 module.exports = filterStateNames;
