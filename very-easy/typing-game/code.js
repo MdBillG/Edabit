68b2ceb5213b7b755b4fce38367c5521fbf4537b
MdBilG <mohammadbilal.g@royalcyber.com>
2025-04-29 17:49:22 +0530

    correctStreeam

--- very-easy/typing-game/code.js
+++ very-easy/typing-game/code.js
@@ -1,5 +1,15 @@
 function correctStream(user, correct) {
 
+    let result = []
+
+    for (let i = 0; i, i < user.length; i++) {
+        if (user[i] === correct[i]) {
+            result.push(1)
+        }
+        else
+            result.push(-1)
+    }
+    return result
 
 }
 
