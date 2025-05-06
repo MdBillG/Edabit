e92ef947c9e76421cecd6c7fdffe55eb12f75b83
MdBillG <user.email>
2025-05-07 02:19:39 +0530

    percentage between

--- easy/calculate-the-percent-difference-between-two-numbers/code.js
+++ easy/calculate-the-percent-difference-between-two-numbers/code.js
@@ -1,6 +1,12 @@
 function percentDiff(num1, num2) {
   // Your code here.
-  s
+
+
+  const difference = Math.abs(num1-num2)
+  const sum = (num1+num2)/2
+
+  return parseFloat(((difference/sum)*100).toFixed(1))
+  
 }
 
 module.exports = percentDiff;
