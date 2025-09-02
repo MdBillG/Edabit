25201d9dc128b214d6363fe003be5a1c12c3d577
MdBillG <Mdb8854@gmail.com>
2025-09-02 19:03:22 +0530

    codeAt

--- easy/capitalize-by-ascii/code.js
+++ easy/capitalize-by-ascii/code.js
@@ -1,6 +1,28 @@
 function asciiCapitalize(str) {
-  // Your code here.
+  // Your co
+  // de here.
 
+// return str.split('').map((ch=>{
+//   const code = ch.charCodeAt(0);
+//   return code % 2===0 ?ch.toUpperCase():ch.toLowerCase()
+// }))
+// .join('')
+
+let res =''
+
+for(let i=0;i<str.length;i++){
+  const ch =str[i]
+  const code = ch.charCodeAt(0)
+  
+
+  if(code%2===0){
+    res+=ch.toUpperCase()
+  }
+  else
+    res+=ch.toLowerCase()
+}
+
+return res
 }
 
 module.exports = asciiCapitalize;
